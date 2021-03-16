62b1694c0a3e0e2c899681b3fe4870bea379853b
sachin569 <sachinjadhav65432@gmail.com>
2021-03-17 00:33:59 +0530

    commit

--- myquiz/settings.py
+++ myquiz/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3nh0t8dlr981qf8@jpbs+5m$%ituuv6mz&s&dornnka*qz2ao$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- myquiz/urls.py
+++ myquiz/urls.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 from myquiz import views as index_views
 from rest_framework.urlpatterns import format_suffix_patterns
 from quizapi import views
+from django.conf import settings
+
+from django.views.static import serve
+from django.conf.urls import url
+
 
 urlpatterns = [
 	url(r'^$', index_views.index),
@@ -10,6 +15,9 @@ urlpatterns = [
 	url(r'^questions/', include('quiz.urls')),
 	url(r'^quizapi/', views.QuizApiList.as_view()),
     url(r'^admin/', admin.site.urls),
+	url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+	
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
